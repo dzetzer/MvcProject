3b073caf0d23b8c32cfc7554b63d2449907c0343
mmosier-web <mmosier5@gmail.com>
2019-10-03 11:25:49 -0400

    began landing page

--- WebApplication4/Startup.cs
+++ WebApplication4/Startup.cs
@@ -32,7 +32,7 @@ namespace WebApplication4
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action = Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
