9a87ce1be92976a8a38792b83c462c312313a0d0
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-10-04 11:45:25 -0400

    Adding Our First Model

---
+++ WebApplication4/Models/Product/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcProject.Models.Product
+{
+    public class Product
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Content { get; set; }
+
+        public Product()
+        {
+        }
+        
+        public Product(string Image, string Title, int ID, string Category, string Manufacter, string Make)
+        {
+
+        }
+}
