fddaffce7750e3c1adf212fdf82db28c22554113
mmosier-web <mmosier5@gmail.com>
2019-10-09 10:08:44 -0400

    added review model

---
+++ WebApplication4/Models/Review/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcProject.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public string UserName { get; set; }
+    }
+}
